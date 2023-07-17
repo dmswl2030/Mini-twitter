a0cc5b418dc8f4e0d303d8bc681eb29a12590d68
EUNJI <eunji9180@naver.com>
2023-07-17 23:33:45 +0900

    :sparkles: Login, Signup 레이아웃

--- pages/create-account.tsx
+++ pages/create-account.tsx
@@ -1,5 +1,6 @@
 import { useRouter } from "next/router";
 import React, { useState } from "react";
+import { NextPage } from "next";
 import { useForm } from "react-hook-form";
 
 interface IForm {
@@ -7,7 +8,7 @@ interface IForm {
   email: string;
 }
 
-export default () => {
+const CreateAccount: NextPage = () => {
   const {
     register,
     handleSubmit,
@@ -39,27 +40,69 @@ export default () => {
     }
   };
   return (
-    <div>
-      <h1>Create Account</h1>
-      <form onSubmit={handleSubmit(onValid)}>
-        <div>
-          <label htmlFor="name">Name: </label>
-          <input
-            type="text"
-            {...register("name", { required: "Write your name please." })}
-          />
-          <span>{errors?.name?.message}</span>
+    <main className="w-full">
+      <div className="max-w-lg m-auto">
+        <div className="w-full min-h-screen">
+          <div className="flex flex-col items-center space-y-20">
+            <div className="p-10">
+              <svg
+                className="w-10 h-10"
+                aria-hidden="true"
+                fill="#b071f5"
+                viewBox="0 0 20 20"
+              >
+                <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84"></path>
+              </svg>
+            </div>
+
+            <div className="w-full space-y-5">
+              <h1 className="font-bold text-2xl">지금 트위터에 가입하세요</h1>
+              <form
+                onSubmit={handleSubmit(onValid)}
+                className="flex flex-col items-center space-y-3"
+              >
+                <div className="w-full">
+                  <input
+                    className="w-full border border-gray-300 rounded-xl h-12 p-2"
+                    type="text"
+                    placeholder="사용자명"
+                    {...register("name", {
+                      required: "Write your name please.",
+                    })}
+                  />
+                  <p className="text-red-500">{errors?.name?.message}</p>
+                </div>
+                <div className="w-full">
+                  <input
+                    className="w-full border border-gray-300 rounded-xl h-12 p-2"
+                    type="email"
+                    placeholder="이메일 주소"
+                    {...register("email", {
+                      required: "Write your email please.",
+                      pattern: {
+                        value: /\S+@\S+\.\S+/,
+                        message: "Please enter a valid email address.",
+                      },
+                    })}
+                  />
+                  <p className="text-red-500">{errors?.email?.message}</p>
+                </div>
+                <button className="bg-customPurple w-full font-bold text-white rounded-2xl h-9">
+                  Sign Up
+                </button>
+              </form>
+              <p>
+                이미 계정이 있으신가요?{" "}
+                <a className="text-blue-500" href="/login">
+                  로그인하기
+                </a>
+              </p>
+            </div>
+          </div>
         </div>
-        <div>
-          <label htmlFor="email">Email: </label>
-          <input
-            type="email"
-            {...register("email", { required: "Write your email please." })}
-          />
-          <span>{errors?.email?.message}</span>
-        </div>
-        <button>Create Account</button>
-      </form>
-    </div>
+      </div>
+    </main>
   );
 };
+
+export default CreateAccount;
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,49 +1,84 @@
-import { useRouter } from "next/router";
-import React, { useState } from "react";
+import React from "react";
 import { useForm } from "react-hook-form";
+import { NextPage } from "next";
+import useMutation from "../libs/client/useMutation";
 
 interface IForm {
+  name: string;
   email: string;
 }
 
-export default () => {
+interface MutationResult {
+  ok: boolean;
+}
+
+const Login: NextPage = () => {
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm<IForm>();
-  const [loading, setLoading] = useState(false);
-  const router = useRouter();
-  const onValid = async (data: IForm) => {
-    if (!loading) {
-      const request = await fetch("/api/users/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-      });
-      if (request.status === 200) {
-        router.push("/");
-      } else {
-        setLoading(false);
-      }
-    }
+  const [login, { loading, data }] =
+    useMutation<MutationResult>("/api/users/login");
+
+  const onValid = (validForm: IForm) => {
+    if (loading) return;
+    login(validForm);
   };
+
   return (
-    <div>
-      <h1>Login</h1>
-      <form onSubmit={handleSubmit(onValid)}>
-        <div>
-          <label htmlFor="email">Email: </label>
-          <input
-            type="email"
-            {...register("email", { required: "Write your email please." })}
-          />
-          <span>{errors?.email?.message}</span>
+    <main className="w-full">
+      <div className="max-w-lg m-auto">
+        <div className="w-full min-h-screen">
+          <div className="flex flex-col items-center space-y-20">
+            <div className="p-10">
+              <svg
+                className="w-10 h-10"
+                aria-hidden="true"
+                fill="#b071f5"
+                viewBox="0 0 20 20"
+              >
+                <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84"></path>
+              </svg>
+            </div>
+
+            <div className="w-full space-y-5">
+              <h1 className="font-bold text-2xl">트위터에 로그인하세요</h1>
+              <form
+                onSubmit={handleSubmit(onValid)}
+                className="flex flex-col items-center space-y-3"
+              >
+                <div className="w-full">
+                  <input
+                    className="w-full border border-gray-300 rounded-xl h-12 p-2"
+                    type="email"
+                    placeholder="이메일 주소"
+                    {...register("email", {
+                      required: "Write your email please.",
+                      pattern: {
+                        value: /\S+@\S+\.\S+/,
+                        message: "Please enter a valid email address.",
+                      },
+                    })}
+                  />
+                  <p className="text-red-500">{errors?.email?.message}</p>
+                </div>
+                <button className="bg-customPurple w-full font-bold text-white rounded-2xl h-9">
+                  Log In
+                </button>
+              </form>
+              <p>
+                계정이 없으신가요?{" "}
+                <a className="text-blue-500" href="/create-account">
+                  가입하기
+                </a>
+              </p>
+            </div>
+          </div>
         </div>
-        <button>Login</button>
-      </form>
-    </div>
+      </div>
+    </main>
   );
 };
+
+export default Login;
